63ecc89ded4727fb1e61c6d1c7d66136fdafe659
PratimaGurav <prtmgrv2@gmail.com>
2021-09-12 15:32:47 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion () {
+
+}
+
+function displaySubtractQuestion () {
+
+}
+
+function displayMultiplyQuestion () {
+
+}
+
+
